6cf22ae69af57d815e36dc2a5af230431fff5a53
youngk1019 <1186354278@qq.com>
2022-04-10 23:17:11 +0800

    add randstring and change rand create

--- util/rand.go
+++ util/rand.go
@@ -2,15 +2,43 @@ package util
 
 import (
 	"crypto/md5"
-	"crypto/rand"
+	"math/rand"
+	"time"
 )
+
+var r *rand.Rand
+
+func init() {
+	r = rand.New(rand.NewSource(time.Now().Unix()))
+}
 
 func RandByte(len uint64) []byte {
 	b := make([]byte, len)
-	_, _ = rand.Read(b)
+	_, _ = r.Read(b)
 	return b
 }
 
 func RandMd5() []byte {
 	return Md5ToBytes(md5.Sum(RandByte(md5.BlockSize)))
+}
+
+var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func RandString(n int, allowedChars ...[]rune) string {
+	var letters []rune
+
+	if len(allowedChars) == 0 {
+		letters = defaultLetters
+	} else {
+		for _, chs := range allowedChars {
+			letters = append(letters, chs...)
+		}
+	}
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[r.Intn(len(letters))]
+	}
+
+	return string(b)
 }
